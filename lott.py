fbca37683cb0c89854cfc52f1b4fad6d61cbe589
Niinjon <87705387+Niinjon@users.noreply.github.com>
2024-09-10 12:27:38 +0200

    Add files via upload

--- lott.py
+++ lott.py
@@ -1,7 +1,25 @@
 import math
 #szám nagyobb mint 5 listázzuk ki az összes olyan 5öst ami különbözik egymástól 
 
-def kereses():
-    
+'''
+szam = int(input("Adja meg a kezdő értéket"))
+szam > 5
 
+az=[1,2,3,4,5]  az[1]+=1 while az[1] == 90
+'''
+
+szam = int(input("Kezdoertek ( >5 ):"))
+
+ez=[]
+az=[]
+
+while ez[]:
+    if szam > 5:
+        for i in range(5):
+            szam += 1
+            az.insert(0,szam)
+            if az[0] == 90:
+                pass
+    else:
+        szam = int(input("Kezdoertek ( >5 ):"))
 
